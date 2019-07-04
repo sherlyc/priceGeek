d1899c905f3ff8ffbbac297207ed6c14776c7b9f
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-07-04 16:48:04 +1200

    update terraform

--- terraform/pricegeek.tf
+++ terraform/pricegeek.tf
@@ -15,6 +15,32 @@ resource "aws_s3_bucket" "www" {
 
 }
 
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  enabled = false
+  default_cache_behavior {
+    allowed_methods = ["GET", "HEAD", "OPTIONS"]
+    cached_methods = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = "www"
+    viewer_protocol_policy = "allow-all"
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+  origin {
+    domain_name = aws_s3_bucket.www.bucket_regional_domain_name
+    origin_id = "www"
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  viewer_certificate {}
+}
 provider "aws" {
     region = "ap-southeast-2"
 }
