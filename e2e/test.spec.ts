7091a838d25955f802739ea4ab99d8d348288f0a
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-07-31 17:40:32 +1200

    add e2e test for dev and prod environments

---
+++ e2e/test.spec.ts
@@ -0,0 +1,47 @@
+import {Browser, launch, Page} from 'puppeteer';
+
+jest.setTimeout(30000);
+
+describe('test', () => {
+    let browser: Browser;
+    let page: Page;
+
+    beforeEach(async () => {
+        page = await browser.newPage();
+    });
+    beforeAll(async () => {
+        browser = await launch();
+
+    });
+    describe('dev homepage', () => {
+        const devHomepage = 'https://d2mc8xylnixdyv.cloudfront.net';
+
+        it('should display Hello world!', async () => {
+            await page.goto(devHomepage);
+            await page.waitForSelector('.content');
+            const textContent = await page.evaluate(() => document.querySelector('.content')!.textContent);
+
+            await expect(textContent).toMatch('Welcome to PriceGeek!')
+        });
+    });
+
+    describe('prod homepage', () => {
+        const prodHomepage = 'https://d13bcdzaghwlha.cloudfront.net';
+
+        it('should display Hello world!', async () => {
+            await page.goto(prodHomepage);
+            await page.waitForSelector('.content');
+            const textContent = await page.evaluate(() => document.querySelector('.content')!.textContent);
+
+            await expect(textContent).toMatch('Welcome to PriceGeek!')
+        });
+    });
+
+    afterEach(async() => {
+        await page.close();
+    });
+
+    afterAll(async() => {
+        await browser.close();
+    });
+});
