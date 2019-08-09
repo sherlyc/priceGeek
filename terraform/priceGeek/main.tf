160fb141012e6fdc21169ae827b2dd057550a4dc
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-08-09 16:30:43 +1200

    extract resources from the main terraform file

--- terraform/priceGeek/main.tf
+++ terraform/priceGeek/main.tf
@@ -1,21 +1,3 @@
-variable "env" {
-  type = string
-}
-
-resource "aws_s3_bucket" "www" {
-  bucket = "${var.env}-pricegeekwww"
-  acl = "private"
-  tags = {
-    terraform="true"
-    }
-
-}
-
-resource "aws_s3_bucket_policy" "site_policy" {
-  bucket = aws_s3_bucket.www.bucket
-  policy = data.aws_iam_policy_document.site_policy_data.json
-}
-
 data "aws_iam_policy_document" "site_policy_data" {
   statement {
     effect = "Allow"
@@ -33,51 +15,10 @@ data "aws_iam_policy_document" "site_policy_data" {
   }
 }
 
-
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  enabled = true
-  default_root_object = "index.html"
-  default_cache_behavior {
-    allowed_methods = ["GET", "HEAD", "OPTIONS"]
-    cached_methods = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = "www"
-    viewer_protocol_policy = "allow-all"
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
-  }
-  origin {
-    domain_name = aws_s3_bucket.www.bucket_domain_name
-    origin_id = "www"
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.site-access-s3.cloudfront_access_identity_path
-    }
-  }
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-}
 provider "aws" {
     region = "ap-southeast-2"
 }
 
-resource "aws_cloudfront_origin_access_identity" "site-access-s3" {
-  comment ="access_from_cloudfront"
-}
 
-resource "aws_s3_bucket_object" "index" {
-  bucket = aws_s3_bucket.www.bucket
-  key = "index.html"
-  source = "../../index.html"
-  content_type = "text/html"
-  etag = "${md5(file("../../index.html"))}"
-}
+
 
---
+++ terraform/priceGeek/s3.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "www" {
+  bucket = "${var.env}-pricegeekwww"
+  acl = "private"
+  tags = {
+    terraform="true"
+  }
+
+}
+
+resource "aws_s3_bucket_policy" "site_policy" {
+  bucket = aws_s3_bucket.www.bucket
+  policy = data.aws_iam_policy_document.site_policy_data.json
+}
+
+resource "aws_s3_bucket_object" "index" {
+  bucket = aws_s3_bucket.www.bucket
+  key = "index.html"
+  source = "../../index.html"
+  content_type = "text/html"
+  etag = "${md5(file("../../index.html"))}"
+}
