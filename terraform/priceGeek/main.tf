6944590ce13fdf994df12d5f3bb5c98110a781a5
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-08-22 16:55:50 +1200

    fixing terraform

--- terraform/priceGeek/main.tf
+++ terraform/priceGeek/main.tf
@@ -1,23 +1,34 @@
-data "aws_iam_policy_document" "site_policy_data" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "s3:GetObject"]
-    resources = [
-      "${aws_s3_bucket.www.arn}/*"]
+terraform {
+  required_version = "~> 0.12"
 
-    principals {
-      identifiers = [
-        aws_cloudfront_origin_access_identity.site-access-s3.iam_arn
-      ]
-      type = "AWS"
-    }
+  backend "s3" {
+    encrypt = true
   }
+
+
 }
 
 provider "aws" {
-    region = "ap-southeast-2"
+  region = "ap-southeast-2"
 }
+
+//data "aws_iam_policy_document" "site_policy_data" {
+//  statement {
+//    effect = "Allow"
+//    actions = [
+//      "s3:GetObject"]
+//    resources = [
+//      "${aws_s3_bucket.www.arn}/*"]
+//
+//    principals {
+//      identifiers = [
+//        aws_cloudfront_origin_access_identity.site-access-s3.iam_arn
+//      ]
+//      type = "AWS"
+//    }
+//  }
+//}
+
 
 
 
