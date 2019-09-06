385360d052ac182185bd2041ad1eca440dbef08e
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-09-06 14:59:20 +1200

    support displaying products info on site after reading from products.json

--- src/client/main.js
+++ src/client/main.js
@@ -6,4 +6,22 @@ fetch("products.json")
 
 function receiveProducts(products) {
     console.log(products)
+    var productsContainer = document.getElementById("products");
+    productsContainer.append(getProductHtml(products[0]));
+}
+
+function getProductHtml(product) {
+    var productDiv = document.createElement("div");
+    var price = document.createElement("h1");
+    var imageWrapper = document.createElement("div");
+    var image = document.createElement("img");
+
+    image.src = product.image;
+    price.innerText = "Price of " + product.name + " is about $"+ product.price;
+
+    imageWrapper.append(image);
+    productDiv.append(price);
+    productDiv.append(imageWrapper);
+
+    return productDiv;
 }
