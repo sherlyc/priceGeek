f184054d380d21e77e987cbf5808280f456145e5
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-09-12 16:44:36 +1200

    add dynamic charts

--- src/client/main.js
+++ src/client/main.js
@@ -8,21 +8,37 @@ function receiveProducts(products) {
     var productsContainer = document.getElementById("products");
     products.forEach(function(product){
         productsContainer.append(getProductHtml(product));
+        plotChart(product);
     })
 }
 
 function getProductHtml(product) {
     var productDiv = document.createElement("div");
     var price = document.createElement("h1");
-    var imageWrapper = document.createElement("div");
-    var image = document.createElement("img");
+    var chart = document.createElement("div");
+    chart.id = product.name;
 
-    image.src = product.image;
     price.innerText = "Price of " + product.name + " is about $"+ product.price;
 
-    imageWrapper.append(image);
     productDiv.append(price);
-    productDiv.append(imageWrapper);
+    productDiv.append(chart);
 
     return productDiv;
+}
+
+function plotChart(product) {
+    Highcharts.chart(product.name, {
+        title: {
+            text: 'Price of '+ product.name
+        },
+
+        tooltip: {
+            valueDecimals: 2
+        },
+        series: [{
+            data: product.chart,
+            lineWidth: 0.5,
+            name: 'Price'
+        }]
+    });
 }
