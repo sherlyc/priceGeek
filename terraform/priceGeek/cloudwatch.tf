d956c753e3e3ba4fa2c353bd3733fdabefc19927
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-11-29 23:24:03 +1300

    fix terraform arn

--- terraform/priceGeek/cloudwatch.tf
+++ terraform/priceGeek/cloudwatch.tf
@@ -1,19 +1,19 @@
-resource "aws_cloudwatch_event_rule" "every_twelve_hours_webScraper" {
-  name = "every-twelve-hours-webScraper"
+resource "aws_cloudwatch_event_rule" "twelve_hours_webscraper" {
+  name = "twelve-hours-webscraper-${var.env}"
   description = "Fires every 12 hours"
   schedule_expression = "rate(12 hours)"
 }
 
-resource "aws_cloudwatch_event_target" "webScraper_every_twelve_hours" {
-  rule = aws_cloudwatch_event_rule.every_twelve_hours_webScraper.name
+resource "aws_cloudwatch_event_target" "webscraper_twelve_hours" {
+  rule = "aws_cloudwatch_event_rule.every_twelve_hours_webscraper_${var.env}.name"
   target_id = "webScraper"
-  arn = aws_lambda_function.webScraper.arn
+  arn = "aws_lambda_function.webscraper_${var.env}.arn"
 }
 
-resource "aws_lambda_permission" "allow_cloudwatch_to_call_webScraper" {
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_webscraper" {
   statement_id = "AllowExecutionFromCloudWatch"
   action = "lambda:InvokeFunction"
   function_name = aws_lambda_function.webScraper.function_name
   principal = "events.amazonaws.com"
-  source_arn = aws_cloudwatch_event_rule.every_twelve_hours_webScraper.arn
+  source_arn = aws_cloudwatch_event_rule.twelve_hours_webscraper.arn
 }
--- terraform/priceGeek/scraperLambda.tf
+++ terraform/priceGeek/scraperLambda.tf
@@ -5,16 +5,16 @@ data "archive_file" "init" {
 }
 
 resource "aws_lambda_function" "webScraper" {
-  function_name = "webScraper"
+  function_name = "webScraper-${var.env}"
   filename = data.archive_file.init.output_path
   handler = "index.handler"
-  role = aws_iam_role.webScraperRole.arn
+  role = "aws_iam_role.webScraperRole_${var.env}.arn"
   runtime = "nodejs10.x"
   timeout = 30
 }
 
 resource "aws_iam_role" "webScraperRole" {
-  name = "webScraperRole"
+  name = "webScraperRole-${var.env}"
   assume_role_policy = data.aws_iam_policy_document.lambdaPolicy.json
 }
 
