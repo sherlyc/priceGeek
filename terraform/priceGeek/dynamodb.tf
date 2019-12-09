061926ca3e15e629bdc66d805a24dede7127cf52
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2019-12-09 22:50:44 +1300

    update dynamodb table

--- terraform/priceGeek/dynamodb.tf
+++ terraform/priceGeek/dynamodb.tf
@@ -1,43 +1,21 @@
-resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = "GameScores${var.env}"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "UserId"
-  range_key      = "GameTitle"
+resource "aws_dynamodb_table" "product_table" {
+  name           = "Products-${var.env}"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "ProductId"
+  range_key      = "ProductKey"
 
   attribute {
-    name = "UserId"
+    name = "ProductId"
     type = "S"
   }
 
   attribute {
-    name = "GameTitle"
+    name = "ProductKey"
     type = "S"
-  }
-
-  attribute {
-    name = "TopScore"
-    type = "N"
-  }
-
-  ttl {
-    attribute_name = "TimeToExist"
-    enabled        = false
-  }
-
-  global_secondary_index {
-    name               = "GameTitleIndex"
-    hash_key           = "GameTitle"
-    range_key          = "TopScore"
-    write_capacity     = 10
-    read_capacity      = 10
-    projection_type    = "INCLUDE"
-    non_key_attributes = ["UserId"]
   }
 
   tags = {
-    Name        = "dynamodb-table-1"
+    Name        = "products-table"
     Environment = var.env
   }
 }
