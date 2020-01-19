a9ebec968c24f13fb47cfde04f30b6a8aadb45f8
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2020-01-19 18:30:46 +1300

    add aws-sdk and listItems from dynamodb table

--- src/server/webScraper/index.ts
+++ src/server/webScraper/index.ts
@@ -1,7 +1,34 @@
+import aws from 'aws-sdk';
 import axios from 'axios';
 import cheerio from 'cheerio';
 
+const docClient = new aws.DynamoDB.DocumentClient({ region: 'ap-southeast-2'});
+
+var params = {
+    TableName: 'ProductScraper-dev'
+};
+
+docClient.scan(params, function(err, data) {
+    if (err) console.log(err)
+    else console.log(data)
+});
+
+async function listItems() {
+    var params = {
+        TableName: 'ProductScraper-dev'
+    }
+
+    try {
+        const data = await docClient.scan(params).promise()
+        return data
+    } catch (err) {
+        return err
+    }
+}
+
 export const handler = async () => {
+    const data = await listItems()
+    console.log('data', data);
     const url = 'https://www.mightyape.co.nz/product/ps4-slim-1tb-value-bundle-ps4/25921903';
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
