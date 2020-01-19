0b2ff514707b6083a5fb6ccc5eda5c9f93e03fed
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2020-01-19 21:07:48 +1300

    update listItems

--- src/server/webScraper/index.ts
+++ src/server/webScraper/index.ts
@@ -8,33 +8,29 @@ var params = {
     TableName: 'ProductScraper-dev'
 };
 
-docClient.scan(params, function(err, data) {
-    if (err) console.log(err)
-    else console.log(data)
-});
-
 async function listItems() {
     var params = {
         TableName: 'ProductScraper-dev'
     }
 
     try {
-        const data = await docClient.scan(params).promise()
-        return data
+        const data = await docClient.scan(params).promise();
+        console.log('list Items', data);
+        return data;
     } catch (err) {
-        return err
+        return err;
     }
 }
 
 export const handler = async () => {
     const data = await listItems();
     console.log("###################")
-    console.log('data', data);
+    console.log('data:', data);
     const url = 'https://www.mightyape.co.nz/product/ps4-slim-1tb-value-bundle-ps4/25921903';
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
     const pricing = $('div.page.listing div.pricing-stock span.price').text().trim();
-    console.info(pricing);
+    console.info('price:', pricing);
 };
 
 
