8f93453d275227e35f3b6329cb5d0b65580e7a6f
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2020-03-12 18:06:44 +1300

    save pricing info into table

--- src/server/webScraper/index.ts
+++ src/server/webScraper/index.ts
@@ -5,12 +5,12 @@ import cheerio from 'cheerio';
 const docClient = new aws.DynamoDB.DocumentClient({ region: 'ap-southeast-2'});
 const env = process.env.environment;
 
-async function getItem() {
+async function getItem(productId: string, vendorId: string) {
     const params = {
         TableName: `ProductScraper-${env}`,
         Key: {
-            "ProductId": 1001,
-            "VendorId": 1
+            "ProductId": productId,
+            "VendorId": vendorId
         }
     };
 
@@ -24,13 +24,38 @@ async function getItem() {
     }
 }
 
+async function putItem(productId: string, vendorId: string, price: string) {
+    const params = {
+        TableName: `ProductScraper-${env}`,
+        Item: {
+            "ProductId_VendorId": `${productId}_${vendorId}`,
+            "Timestamp": new Date().toISOString(),
+            "Price": price
+        }
+    };
+
+    try {
+        const data = await docClient.put(params).promise();
+        console.log('put item', data);
+        return data;
+    } catch (err) {
+        console.error("Unable to put item. Error JSON:", JSON.stringify(err, null, 2));
+        return err;
+    }
+}
+
 export const handler = async () => {
-    const data = await getItem();
+    const productId = "1001";
+    const vendorId = "1";
+    const data = await getItem(productId, vendorId);
+
     if(data) {
         const response = await axios.get(data.Item.Url);
         const $ = cheerio.load(response.data);
         const pricing = $('div.pricing-stock div.price span.price').text().trim();
         console.info('item', JSON.stringify(data, null, 2));
+        await putItem(productId, vendorId, pricing);
+
         return pricing;
     }
     console.log('no data, exiting')
