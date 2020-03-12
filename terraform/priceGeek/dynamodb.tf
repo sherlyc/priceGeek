90a2a0a08a268dadd0a85f79e3727fe4dc59af73
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2020-03-12 18:05:56 +1300

    add vendor and product history tables

--- terraform/priceGeek/dynamodb.tf
+++ terraform/priceGeek/dynamodb.tf
@@ -28,17 +28,56 @@ resource "aws_dynamodb_table" "products_scraper" {
 
   attribute {
     name = "ProductId"
-    type = "N"
+    type = "S"
   }
 
   attribute {
     name = "VendorId"
-    type = "N"
+    type = "S"
   }
 
   tags = {
     Name        = "products-scraper"
     Environment = var.env
+  }
+}
+
+resource "aws_dynamodb_table" "products_history" {
+  name = "ProductHistory-${var.env}"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "ProductId_VendorId"
+  range_key      = "Timestamp"
+
+  attribute {
+    name = "ProductId_VendorId"
+    type = "S"
+  }
+
+  attribute {
+    name = "Timestamp"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "products-history"
+    Environment = var.env
+  }
+}
+
+resource "aws_dynamodb_table" "vendors" {
+  name = "Vendors-${var.env}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "VendorId"
+  range_key = "ProductId"
+
+  attribute {
+    name = "VendorId"
+    type = "S"
+  }
+
+  attribute {
+    name = "ProductId"
+    type = "S"
   }
 }
 
