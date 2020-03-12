ffcc2e870fd4794f495ea5f0bc3b712902779e9b
Sherly Chan <sherly.chan@fairfaxmedia.co.nz>
2020-03-12 23:25:12 +1300

    reset products scraper table

--- terraform/priceGeek/dynamodb.tf
+++ terraform/priceGeek/dynamodb.tf
@@ -20,6 +20,40 @@ resource "aws_dynamodb_table" "product_table" {
   }
 }
 
+resource "aws_dynamodb_table" "products_scraper" {
+  name           = "ProductScraper-${var.env}"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "ProductId"
+  range_key      = "VendorId"
+
+  attribute {
+    name = "ProductId"
+    type = "S"
+  }
+
+  attribute {
+    name = "VendorId"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "products-scraper"
+    Environment = var.env
+  }
+}
+
+resource "aws_dynamodb_table_item" "products_scraper" {
+  table_name = aws_dynamodb_table.products_scraper.name
+  hash_key = aws_dynamodb_table.products_scraper.hash_key
+
+  item = <<ITEM
+{
+  "ProductId": {"S": "1001"},
+  "VendorId": {"S": "1"}
+}
+ITEM
+}
+
 resource "aws_dynamodb_table" "products_history" {
   name = "ProductHistory-${var.env}"
   billing_mode   = "PAY_PER_REQUEST"
